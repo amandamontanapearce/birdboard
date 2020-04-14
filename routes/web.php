b6ff055d7c748b35a93f8e83daea95d3b0f63d17
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2020-04-14 13:57:13 -0600

    refactor to controller

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/projects', function () {
-    $projects = App\Project::all();
-    return view('/projects.index', compact('projects'));
-});
+Route::get('/projects', 'ProjectsController@index');
 
-Route::post('/projects', function () {
-    //validate
-    //persist
-    App\Project::create(request(['title', 'description']));
-    //create
-});
+Route::post('/projects', 'ProjectsController@store');
