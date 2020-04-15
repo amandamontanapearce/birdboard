f4423f01dd571592111b12735d6216a63d2a4c16
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2020-04-15 16:24:32 -0600

    add validation tests, factory, and show

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -12,11 +12,20 @@ class ProjectsController extends Controller
         $projects = Project::all();
         return view('/projects.index', compact('projects'));
     }
+
+    public function show()
+    {
+        $project = Project::findOrFail(request('project'));
+        return view('/projects.show', compact('project'));
+    }
+
     public function store()
     {
         //validate
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
         //persist
-        Project::create(request(['title', 'description']));
-        //create
+        Project::create($attributes);
+        //redirect
+        return redirect('/projects');
     }
 }
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,7 +11,7 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /**
-     * A basic test example.
+     * create project.
      *
      * @return void
      */
@@ -23,8 +23,45 @@ class ProjectsTest extends TestCase
             'title' => $this->faker->sentence,
             'description' => $this->faker->sentence,
         ];
-        $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
         $this->assertDatabaseHas('projects', $attributes);
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /**
+     * view a project
+     *
+     * @return void
+     */ 
+    public function testUserCanViewProject()
+    {
+        $this->withoutExceptionHandling();
+        
+        $project = factory('App\Project')->create();
+        $this->get('/projects/' . $project->id)
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+    /**
+     * validate project
+     *
+     * @return void
+     */
+    public function testProjectRequiresATitle()
+    {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+        /**
+     * validate project
+     *
+     * @return void
+     */
+    public function testProjectRequiresADescription()
+    {
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
